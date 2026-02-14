9b89e09fb7dc045d15b5a4c325328434c2baa929
rai <96561881+r4ai@users.noreply.github.com>
2026-02-15 01:52:37 +0900

    chore: init tanstack router

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,28 @@
-import React from "react"
+import { createRouter, RouterProvider } from "@tanstack/react-router"
+import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App"
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// Import the generated route tree
+import { routeTree } from "./routeTree.gen"
+
+// Create a new router instance
+const router = createRouter({ routeTree })
+
+// Register the router instance for type safety
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// Render the app
+// biome-ignore lint/style/noNonNullAssertion: #root is guaranteed to exist in our index.html
+const rootElement = document.getElementById("root")!
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+  root.render(
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>,
+  )
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { tanstackRouter } from "@tanstack/router-plugin/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
@@ -6,7 +7,13 @@ const host = process.env.TAURI_DEV_HOST
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
